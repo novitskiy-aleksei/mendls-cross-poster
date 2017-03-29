fc4935dc084cafa98c20f50d13d931880ad84f9b
Aleksei Novitskiy <onovitskyi@oroinc.com>
2017-03-29 03:38:07 +0300

    Initial commit

---
+++ app/BlogArticleDescription.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogArticleDescription extends Model
+{
+    //
+}
---
+++ app/Providers/InstaSyncProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use InstagramScraper\Instagram;
+
+class InstaSyncProvider extends AppServiceProvider
+{
+    public function fetchLatestPosts()
+    {
+
+    }
+}
